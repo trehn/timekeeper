f327d4aacc0db9b5ad897fab0d2af830ac734a55
Torsten Rehn <torsten@rehn.email>
2015-05-31 14:31:30 +0200

    Measure what is measurable, and make measurable what is not so.

---
+++ timekeeper.py
@@ -0,0 +1,47 @@
+from contextlib import contextmanager
+from datetime import datetime
+
+from influxdb import InfluxDBClient
+
+
+@contextmanager
+def timekeeper_context(tk, measurement, tags):
+    start = datetime.now()
+    yield
+    end = datetime.now()
+    tk.submit_measurement(
+        measurement,
+        (end - start).total_seconds(),
+        tags,
+    )
+
+
+class TimeKeeper(object):
+    def __init__(self, dsn, prefix="", tags=None, **kwargs):
+        self.connection = InfluxDBClient.from_DSN(dsn, **kwargs)
+        self.prefix = prefix
+        self.tags = {} if tags is None else tags
+
+    def context(self, measurement, tags=None):
+        tags = {} if tags is None else tags
+        return timekeeper_context(self, measurement, tags)
+
+    def decorate(self, measurement, tags=None):
+        def outer(func):
+            def inner(*args, **kwargs):
+                with self.context(measurement, tags=tags):
+                    return func(*args, **kwargs)
+            return inner
+        return outer
+
+    def submit_measurement(self, measurement, value, tags):
+        self.connection.write_points(
+            [{
+                "fields": {
+                    "value": value,
+                },
+                "name": self.prefix + measurement,
+                "tags": tags,
+            }],
+            tags=self.tags,
+        )
